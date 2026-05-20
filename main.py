8ae7c0b826df4a996a7af45663c6ed36ccb02974
gabrieelvillamayor-debug <gabrieelvillamayor@gmail.com>
2026-05-19 22:52:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,46 +5,64 @@ from deep_translator import GoogleTranslator
 PAGE_ID = "1073487005856687"
 PAGE_TOKEN = "EAASlKZCWsRZBMBRkXVPGyOJuUwZCIhuIMyO5dxpiFMbH6XJRmqitnoXb5OTf9oAO8R20ATvpHWny0OnZBYqzkbgqvOZATjo2CvSKg3w4CDQPlAWIAZB6RcuBEbdzl1YO775MdHd8Gcy0HGJPZB6QHfB1m8ix0fzfhOBZBw7d4BRf2BZCbKAxzNtfeIY2uRCfxZC4WmLTkFvys45aO3kuLGaV2PKbZAbkleHPsSyglq7BtoZD"
 
-# Obtener curiosidad random
-fact = requests.get(
-    "https://uselessfacts.jsph.pl/api/v2/facts/random"
-).json()
+headers = {
+    "User-Agent": "ByteDiarioBot"
+}
 
-dato_ingles = fact["text"]
-
-# Traducir al español
-dato = GoogleTranslator(
-    source='auto',
-    target='es'
-).translate(dato_ingles)
-
-titulos = [
-    "🤯 DATO QUE TE VOLARÁ LA CABEZA",
-    "😳 ESTO ES REAL",
-    "🧠 CURIOSIDAD DEL DÍA",
-    "👀 CASI NADIE SABE ESTO",
-    "🔥 DATO IMPACTANTE"
+subreddits = [
+    "todayilearned",
+    "Damnthatsinteresting",
+    "interestingasfuck"
 ]
 
-titulo = random.choice(titulos)
+subreddit = random.choice(subreddits)
+
+url_reddit = f"https://www.reddit.com/r/{subreddit}/top.json?t=day&limit=15"
+
+data = requests.get(
+    url_reddit,
+    headers=headers
+).json()
+
+posts = data["data"]["children"]
+
+post_random = random.choice(posts)
+
+titulo_ingles = post_random["data"]["title"]
+
+titulo = GoogleTranslator(
+    source='auto',
+    target='es'
+).translate(titulo_ingles)
+
+mensajes = [
+    "🤯 ESTO ES REAL",
+    "🧠 DATO VIRAL DEL DÍA",
+    "😳 CASI NADIE SABE ESTO",
+    "🔥 INTERNET ESTÁ HABLANDO DE ESTO",
+    "👀 MIRA ESTE DATO"
+]
+
+encabezado = random.choice(mensajes)
 
 mensaje = f"""
-{titulo}
+{encabezado}
 
-{dato}
+{titulo}
 
 ¿Lo sabías? 👀
 
-#Curiosidades #DatosCuriosos #SabiasQue #ByteDiario
+#Curiosidades #DatosCuriosos #ByteDiario
 """
 
-url = f"https://graph.facebook.com/{PAGE_ID}/feed"
+url_fb = f"https://graph.facebook.com/{PAGE_ID}/feed"
 
 payload = {
     "message": mensaje,
     "access_token": PAGE_TOKEN
 }
 
-r = requests.post(url, data=payload)
+r = requests.post(url_fb, data=payload)
 
 print(r.text)
+
